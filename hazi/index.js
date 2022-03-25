f4cc6f9148997a07b91d7803e4893d2bb18c4a22
Bakucz Jonatán Márk <jonatanbakucz@gmail.com>
2022-03-25 18:48:38 +0100

    created server

--- hazi/index.js
+++ hazi/index.js
@@ -3,3 +3,8 @@ var app = express();
 
 //Serve static before session
 app.use(express.static('public'));
+
+//Server
+var server = app.listen(8000, function () {
+    console.log('Server running at :8000');
+});
