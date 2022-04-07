2465f43c0928ae294b5554ebf882587c21d40553
Bakucz Jonatán Márk <jonatanbakucz@gmail.com>
2022-04-07 23:42:57 +0200

    negyedik hazi kesz

--- hazi/index.js
+++ hazi/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 //Serve static before session
 app.use(express.static('public'));
+
+require('./routes/route')(app);
 
 //Server
 var server = app.listen(8000, function () {
